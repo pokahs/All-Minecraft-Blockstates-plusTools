c36a9b2a85ccd3f26dc22155e622bcab636a0850
pokahs <fart77480@gmail.com>
2023-10-13 14:14:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-#from MinecraftTools import worldEdit
-
-#worldEdit.replaceCommand.newSet()
-
-
-#from MinecraftTools import blockStateParsing
-
-
-#blockStateParsing.optimizing_load(json_input="themotherload.json", json_output="block_data.json")
-
-#blockStateParsing.grouping_load(json_input="block_data.json", json_output="group_data.json")
-
-#blockStateParsing.addgroupatr_load(json_input="group_data.json", json_output="block_data.json")
+# Nothing here yet :P stay tuned
 
